39c12caec51e91347dcc6dcbb4f0d416253dbbc4
Elton Leão <elton.leao@peopleinteractive.com.br>
2024-01-08 17:23:32 -0300

    Add Comment component and comment list to Post component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={`${styles.comment}`}>
+      <img src="https://picsum.photos/200" alt="" />
+
+      <div className={`${styles.commentBox}`}>
+        <div className={`${styles.commentContent}`}>
+          <header>
+            <div className={`${styles.authorAndTime}`}>
+              <strong>Lorem Ipsum</strong>
+              <time title="01 de  janeiro de 2024">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={16} />
+            </button>
+          </header>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita,
+            nulla?
+          </p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={16} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
